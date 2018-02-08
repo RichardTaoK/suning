c0eefd93916f14ea3354e3a53646feab0c87dcd1
2579312470@qq.com <2579312470@qq.com>
2018-02-08 17:21:34 +0800

    suning

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ window.onload=function(){
 	let dian=document.querySelectorAll('.ban_tu .dian li');
 	let rightbtn=document.querySelector('.ban_tu .right')
 	let leftbtn=document.querySelector('.ban_tu .left')
-
 	let n=0;
 	function move(){
 		n++;
@@ -115,6 +114,8 @@ window.onload=function(){
 		let sou=document.querySelector('.sousou');
 		if(tops>lw-200){
 			nav.style.display="block";
+		}else{
+			nav.style.display="none";
 		}
 		if(tops>ch*2){
 			if(flag1){
@@ -138,16 +139,87 @@ window.onload=function(){
 			}
 		})
 		navli.forEach(function(value,index){
-			let a=fls[index].offsetTop;
+			// let a=fls[index].offsetTop;
 			value.onclick=function(){
 				flag3=false;
-				animate(document.body,{scrollTop:a}, 600,function(){flag3=true})
-				animate(document.documentElement,{scrollTop:a}, 600,function(){flag3=true})
+				animate(document.body,{scrollTop:fls[index].offsetTop}, 600,function(){flag3=true})
+				animate(document.documentElement,{scrollTop:fls[index].offsetTop}, 600,function(){flag3=true})
 				navli.forEach(function(v,i){
 					v.classList.remove('active');
 				})
 				value.classList.add('active');
 			}
 		})
+	}
+// banner选择卡
+	let lili=document.querySelectorAll('.banner .left li');
+	let zizi=document.querySelectorAll('.banner .left .zi div');
+	lili.forEach(function(value,index1){
+		value.onmouseover=function(){
+			zizi.forEach(function(v,i){
+				v.classList.remove('active');
+			})
+			zizi[index1].classList.add('active');
+		}
+	})
+
+	let fu=document.querySelectorAll('header .fu');
+	let xian=document.querySelectorAll('header .xian');
+	fu.forEach(function(value,index){
+		value.onmousemove=function(){
+			xian.forEach(function(v,i){
+				animate(v,{height:0}, 200);
+			})
+			if(index==fu.length-1){
+				animate(xian[index],{height:224}, 200);
+
+			}else{
+				animate(xian[index],{height:124}, 200);
+			}
+		}
+		value.onmouseout=function(){
+			animate(xian[index],{height:0}, 200);
+
+		}
+	})
+// 节点轮播
+	let small=document.querySelector('.small ul');
+	let smlist=document.querySelectorAll('.small ul li');
+	let left=document.querySelector('.small .left'); 
+	let right=document.querySelector('.small .right');
+	let w=small.parentNode.offsetWidth; 
+	let vadios=document.querySelectorAll('.pindao .shiping a');
+	function am(){
+		animate(small,{left:-w}, 1000,function(){
+			// let i=0;
+			for(let i=0;i<3;i++){
+				let first=small.firstElementChild;
+				small.appendChild(first);
+			}
+				small.style.left=0;
+
+		})
+	}
+	right.onclick=function(){
+		am();
+		
+	}
+	smlist.forEach(function(value,index){
+		value.onmousemove=function(){
+			vadios.forEach(function(v,i){
+				v.classList.remove('active');
+			})
+			vadios[index].classList.add('active');
+		}
+	})
+	left.onclick=function(){
+		small.style.left=-w+'px';
+		for(let i=0;i<3;i++){
+			let first=small.firstElementChild;
+			let last=small.lastChild;
+			small.insertBefore(last, first);
+			
+		}
+		animate(small,{left:0}, 1000)
 	}
 }
